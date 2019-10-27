676946e49ef7055ecdc72b78f07da926c200d2a3
s-prasad <saurabh.prasad@gmail.com>
2019-10-27 11:48:52 -0400

    Create Exercise 1.py

---
+++ UNIT 1/Lecture 2 - Decision Trees and Dynamic Programming/Exercise 1.py
@@ -0,0 +1,22 @@
+def yieldAllCombos(items):
+    """
+        Generates all combinations of N items into two bags, whereby each 
+        item is in one or zero bags.
+
+        Yields a tuple, (bag1, bag2), where each bag is represented as a list 
+        of which item(s) are in each bag.
+    """
+    # Your code here
+    
+    N = len(items)
+    # enumerate the 3**N possible combinations
+    for i in range(3**N):
+        bag1 = []
+        bag2 = []
+        for j in range(N):
+            if ((i // 3**j) % 3 == 1):
+                bag1.append(items[j])
+            elif ((i // 3**j) % 3 == 2):
+                bag2.append(items[j])
+            
+        yield (bag1, bag2)
